df9860666f82e307be9e0f1ac3540cc8f0c4d65b
anjakely18 <anjaniaina03@gmail.com>
2024-05-31 20:22:22 +0200

    title

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Title from "./Title";
+import data from "./data";
+
+const App = () => {
+  return (
+    <main>
+      <section>
+        <Title text={"our menu"} />
+      </section>
+    </main>
+  );
+};
+export default App;
